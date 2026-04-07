eb7beea6896a7f3c1bfb11a6703b150bb1a43d5d
Joaquin Pardiñas <148148167+JoaquinP711@users.noreply.github.com>
2026-04-07 15:35:36 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def health_check():
 
 @app.route('/')
 def hello_world():
-    return jsonify({"mensaje": "¡Hola! Esta es una aplicación de ejemplo para CI/CD"})
+    return jsonify({"message": "¡Hello! This is a sample application for CI/CD."})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
